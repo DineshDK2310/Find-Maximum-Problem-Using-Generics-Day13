5b291a02b72712b0a580b2767b5fc381f9909118
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-02-13 13:11:53 -0800

    Adding:Find Maximum Strings File Added Successfully

---
+++ FindMaximumStrings.java
@@ -0,0 +1,36 @@
+package Generics;
+
+public class FindMaximumStrings <E>{
+	E x, y, z;
+
+	public FindMaximumStrings(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static  <T extends Comparable<T>> String testMaximum(String x, String y, String z) {
+		String maximum = x;
+		if(y.compareTo(maximum) > 0) {
+			maximum = y;
+		}
+
+		if(z.compareTo(maximum) > 0) {
+			maximum = z;
+		}
+		printMaximum(x,y,z,maximum);
+		return maximum;
+	}
+
+	public static void printMaximum(String x, String y, String z, String maximum) {
+		System.out.printf("Max of %s, %s and %s is %s\n",x,y,z, maximum);		
+	}
+
+	public static void main(String[] args) {
+		String xStr= "Apple", yStr = "Peach", zStr = "Banana";
+
+		new FindMaximumStrings(xStr,yStr,zStr).testMaximum(xStr, yStr, zStr);
+	}
+
+
+}
