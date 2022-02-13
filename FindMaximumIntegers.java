b95422519533462c1c2a21af41aa3b676371589d
DineshDK2310 <dineshkumardk101199@gmail.com>
2022-02-13 13:08:59 -0800

    Adding:Find Maximum Integers File Added Successfully

---
+++ FindMaximumIntegers.java
@@ -0,0 +1,35 @@
+package Generics;
+
+public class FindMaximumIntegers <E>{
+	E x, y, z;
+
+	public FindMaximumIntegers(E x, E y, E z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public static  <T extends Comparable<T>> Integer testMaximum(Integer x, Integer y, Integer z) {
+		Integer maximum = x;
+		if(y.compareTo(maximum) > 0) {
+			maximum = y;
+		}
+
+		if(z.compareTo(maximum) > 0) {
+			maximum = z;
+		}
+		printMaximum(x,y,z,maximum);
+		return maximum;
+	}
+
+	public static void printMaximum(Integer x, Integer y, Integer z, Integer maximum) {
+		System.out.printf("Max of %s, %s and %s is %s\n",x,y,z, maximum);		
+	}
+
+	public static void main(String[] args) {
+		Integer xInt= 4, yInt = 6, zInt = 9;
+		new FindMaximumIntegers(xInt,yInt,zInt).testMaximum(xInt, yInt, zInt);
+	}
+
+}
+
